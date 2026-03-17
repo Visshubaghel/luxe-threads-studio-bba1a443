4609db3b895e4f8e96a78750cdaad9863cbd519b
Visshubaghel <Visshubaghel@gmail.com>
2026-03-17 15:14:25 +0530

    your message

---
+++ hello.py
@@ -0,0 +1,1 @@
+hello this is good by the way
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/luxe-threads-studio-bba1a443/",
   server: {
     host: "::",
     port: 8080,
